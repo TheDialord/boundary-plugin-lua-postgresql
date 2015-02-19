d42149443dab00a0055910091b0662aa7ba257e3
Yegor Dia <yegordia@gmail.com>
2015-02-19 05:19:34 +0500

    multiply python&lua instances metrics

--- index.lua
+++ index.lua
@@ -2,74 +2,82 @@ local boundary = require('boundary')
 local pginfo = require('pginfo')
 
 -- Default params
-local host = "localhost"
-local port = 5432
-local user = "postgres"
-local pwd = ""
-local database = "postgres"
-local source = ""
+local connections = {
+[0] = {
+	host = "localhost",
+	port = 5432,
+	user = "postgres",
+	password = "123qwe",
+	database = "postgres",
+	source = ""
+}}
 
 -- Fetching params
 if (boundary.param ~= nil) then
-  host = boundary.param['host'] or host
-  port = boundary.param['port'] or port
-  user = boundary.param['user'] or user
-  pwd = boundary.param['pwd'] or pwd
-  database = boundary.param['database'] or database
-  source = boundary.param['source'] or source
+  connections = boundary.param or connections
 end
 
 print("_bevent:Boundary LUA Postgres plugin up : version 1.0|t:info|tags:lua,plugin")
+local dbcon = nil
 
-local dbcon = pginfo:new(host, port, user, pwd, database, source)
+local function poll(connections)
+	if table.getn(connections) > 0 then
+		local query = connections[1]
+		local host = query.host
+		local port = query.port
+		local user = query.user
+		local password = query.password
+		local database = query.database
+		local source = query.source
+		table.remove(connections, 1)
+		local dbcon = pginfo:new(host, port, user, password, database, source)
 
+		dbcon:establish(function(connection)
+			dbcon:get_databases(connection, function(dbs)
+				dbcon:get_bg_writer_stats(connection, function(writer_stats)
+					dbcon:get_lock_stats_mode(connection, function(db_locks)
+						dbcon:get_database_stats(connection, function(db_stats)
+							
+							--lock stats
+							p(string.format("POSTGRESQL_EXCLUSIVE_LOCKS %s %s", db_locks['all']['Exclusive'], source))
+							p(string.format("POSTGRESQL_ROW_EXCLUSIVE_LOCKS %s %s", db_locks['all']['RowExclusive'], source))
+							p(string.format("POSTGRESQL_SHARE_ROW_EXCLUSIVE_LOCKS %s %s", db_locks['all']['ShareRowExclusive'], source))
+							p(string.format("POSTGRESQL_SHARE_UPDATE_EXCLUSIVE_LOCKS %s %s", db_locks['all']['ShareUpdateExclusive'], source))
+							p(string.format("POSTGRESQL_SHARE_LOCKS %s %s", db_locks['all']['Share'], source))
+							p(string.format("POSTGRESQL_ACCESS_SHARE_LOCKS %s %s", db_locks['all']['AccessShare'], source))
+							
+							--checkpoint/bgwriter stats
+							p(string.format("POSTGRESQL_CHECKPOINT_WRITE_TIME %s %s", writer_stats['checkpoint_write_time'], source))
+							p(string.format("POSTGRESQL_CHECKPOINTS_TIMED %s %s", writer_stats['checkpoints_timed'], source))
+							p(string.format("POSTGRESQL_BUFFERS_ALLOCATED %s %s", writer_stats['buffers_alloc'], source))
+							p(string.format("POSTGRESQL_BUFFERS_CLEAN %s %s", writer_stats['buffers_clean'], source))
+							p(string.format("POSTGRESQL_BUFFERS_BACKEND_FSYNC %s %s", writer_stats['buffers_backend_fsync'], source))
+							p(string.format("POSTGRESQL_CHECKPOINT_SYNCHRONIZATION_TIME %s %s", writer_stats['checkpoint_sync_time'], source))
+							p(string.format("POSTGRESQL_CHECKPOINTS_REQUESTED %s %s", writer_stats['checkpoints_req'], source))
+							p(string.format("POSTGRESQL_BUFFERS_BACKEND %s %s", writer_stats['buffers_backend'], source))
+							p(string.format("POSTGRESQL_MAXIMUM_WRITTEN_CLEAN %s %s", writer_stats['maxwritten_clean'], source))
+							p(string.format("POSTGRESQL_BUFFERS_CHECKPOINT %s %s", writer_stats['buffers_checkpoint'], source))
 
-local function poll()
-	dbcon:establish(function(connection)
-		dbcon:get_databases(connection, function(dbs)
-			dbcon:get_bg_writer_stats(connection, function(writer_stats)
-				dbcon:get_lock_stats_mode(connection, function(db_locks)
-					dbcon:get_database_stats(connection, function(db_stats)
-						
-						--lock stats
-						p(string.format("POSTGRESQL_EXCLUSIVE_LOCKS %s %s", db_locks['all']['Exclusive'], source))
-						p(string.format("POSTGRESQL_ROW_EXCLUSIVE_LOCKS %s %s", db_locks['all']['RowExclusive'], source))
-						p(string.format("POSTGRESQL_SHARE_ROW_EXCLUSIVE_LOCKS %s %s", db_locks['all']['ShareRowExclusive'], source))
-						p(string.format("POSTGRESQL_SHARE_UPDATE_EXCLUSIVE_LOCKS %s %s", db_locks['all']['ShareUpdateExclusive'], source))
-						p(string.format("POSTGRESQL_SHARE_LOCKS %s %s", db_locks['all']['Share'], source))
-						p(string.format("POSTGRESQL_ACCESS_SHARE_LOCKS %s %s", db_locks['all']['AccessShare'], source))
-						
-						--checkpoint/bgwriter stats
-						p(string.format("POSTGRESQL_CHECKPOINT_WRITE_TIME %s %s", writer_stats['checkpoint_write_time'], source))
-						p(string.format("POSTGRESQL_CHECKPOINTS_TIMED %s %s", writer_stats['checkpoints_timed'], source))
-						p(string.format("POSTGRESQL_BUFFERS_ALLOCATED %s %s", writer_stats['buffers_alloc'], source))
-						p(string.format("POSTGRESQL_BUFFERS_CLEAN %s %s", writer_stats['buffers_clean'], source))
-						p(string.format("POSTGRESQL_BUFFERS_BACKEND_FSYNC %s %s", writer_stats['buffers_backend_fsync'], source))
-						p(string.format("POSTGRESQL_CHECKPOINT_SYNCHRONIZATION_TIME %s %s", writer_stats['checkpoint_sync_time'], source))
-						p(string.format("POSTGRESQL_CHECKPOINTS_REQUESTED %s %s", writer_stats['checkpoints_req'], source))
-						p(string.format("POSTGRESQL_BUFFERS_BACKEND %s %s", writer_stats['buffers_backend'], source))
-						p(string.format("POSTGRESQL_MAXIMUM_WRITTEN_CLEAN %s %s", writer_stats['maxwritten_clean'], source))
-						p(string.format("POSTGRESQL_BUFFERS_CHECKPOINT %s %s", writer_stats['buffers_checkpoint'], source))
-
-						--Global DB Stats
-						p(string.format("POSTGRESQL_BLOCKS_READ %s %s", db_stats['totals']['blks_read'], source))
-						p(string.format("POSTGRESQL_DISK_SIZE %s %s", db_stats['totals']['disk_size'], source))
-						p(string.format("POSTGRESQL_TRANSACTIONS_COMMITTED %s %s", db_stats['totals']['xact_commit'], source))
-						p(string.format("POSTGRESQL_TUPLES_DELETED %s %s", db_stats['totals']['tup_deleted'], source))
-						p(string.format("POSTGRESQL_TRANSACTIONS_ROLLEDBACK %s %s", db_stats['totals']['xact_rollback'], source))
-						p(string.format("POSTGRESQL_BLOCKS_HIT %s %s", db_stats['totals']['blks_hit'], source))
-						p(string.format("POSTGRESQL_TUPLES_RETURNED %s %s", db_stats['totals']['tup_returned'], source))
-						p(string.format("POSTGRESQL_TUPLES_FETCHED %s %s", db_stats['totals']['tup_fetched'], source))
-						p(string.format("POSTGRESQL_TUPLES_UPDATED %s %s", db_stats['totals']['tup_updated'], source))
-						p(string.format("POSTGRESQL_TUPLES_INSERTED %s %s", db_stats['totals']['tup_inserted'], source))
-						p(string.format("POSTGRESQL_TUPLES_FETCHED %s %s", db_stats['totals']['tup_fetched'], source))
-						
+							--Global DB Stats
+							p(string.format("POSTGRESQL_BLOCKS_READ %s %s", db_stats['totals']['blks_read'], source))
+							p(string.format("POSTGRESQL_DISK_SIZE %s %s", db_stats['totals']['disk_size'], source))
+							p(string.format("POSTGRESQL_TRANSACTIONS_COMMITTED %s %s", db_stats['totals']['xact_commit'], source))
+							p(string.format("POSTGRESQL_TUPLES_DELETED %s %s", db_stats['totals']['tup_deleted'], source))
+							p(string.format("POSTGRESQL_TRANSACTIONS_ROLLEDBACK %s %s", db_stats['totals']['xact_rollback'], source))
+							p(string.format("POSTGRESQL_BLOCKS_HIT %s %s", db_stats['totals']['blks_hit'], source))
+							p(string.format("POSTGRESQL_TUPLES_RETURNED %s %s", db_stats['totals']['tup_returned'], source))
+							p(string.format("POSTGRESQL_TUPLES_FETCHED %s %s", db_stats['totals']['tup_fetched'], source))
+							p(string.format("POSTGRESQL_TUPLES_UPDATED %s %s", db_stats['totals']['tup_updated'], source))
+							p(string.format("POSTGRESQL_TUPLES_INSERTED %s %s", db_stats['totals']['tup_inserted'], source))
+							p(string.format("POSTGRESQL_TUPLES_FETCHED %s %s", db_stats['totals']['tup_fetched'], source))
+							
+							poll(connections)
+						end)
 					end)
 				end)
 			end)
 		end)
-	end)
+	end
 end
 
-poll()
-
+poll(connections)
--- postgres_plugin.py
+++ postgres_plugin.py
@@ -4,65 +4,87 @@ import json
 from postgresql import PgInfo
 from os import path
 
-if (len(sys.argv) != 7):
-  sys.stderr.write("usage: {0} host port database user password source\n".format(path.basename(sys.argv[0])))
-  sys.exit(1)
+if (len(sys.argv) != 2):
+    sys.stderr.write(
+        'usage: {0} "host port database user password source,host port database user password source, <...>"\n'.format(
+            path.basename(sys.argv[0])))
+    sys.exit(1)
 
-#grab connection info's. This is expected in the Format host/port/db/user/pwd
-_host = sys.argv[1]
-_port = sys.argv[2]
-_database = sys.argv[3]
-_user = sys.argv[4]
-_password = sys.argv[5]
-_source = sys.argv[6]
+# grab connection infos. This is expected in the Format host/port/db/user/pwd for each connection
 
-def poll():       
-	_dbconn = PgInfo(_host, _port, _database, _user, _password)
-	dbs =_dbconn.getDatabases()
-	
-	#global stats
-	writerStats = _dbconn.getBgWriterStats()
-	dbLocks = _dbconn.getLockStatsMode()
-	dbStats = _dbconn.getDatabaseStats() #also gives individual DB stats
-	
-	#per db stats - not used in this version
-	#locksByDB=_dbconn.getLockStatsDB()
-	#connStats=_dbconn.getConnectionStats()
-	
-	#print writerStats
-	#lock stats
-	print("POSTGRESQL_EXCLUSIVE_LOCKS {0} {1}".format(dbLocks['all']['Exclusive'], _source))
-	print("POSTGRESQL_ROW_EXCLUSIVE_LOCKS {0} {1}".format(dbLocks['all']['RowExclusive'], _source))
-	print("POSTGRESQL_SHARE_ROW_EXCLUSIVE_LOCKS {0} {1}".format(dbLocks['all']['ShareRowExclusive'], _source))
-	print("POSTGRESQL_SHARE_UPDATE_EXCLUSIVE_LOCKS {0} {1}".format(dbLocks['all']['ShareUpdateExclusive'], _source))
-	print("POSTGRESQL_SHARE_LOCKS {0} {1}".format(dbLocks['all']['Share'], _source))
-	print("POSTGRESQL_ACCESS_SHARE_LOCKS {0} {1}".format(dbLocks['all']['AccessShare'], _source))
-	
-	#checkpoint/bgwriter stats
-	print("POSTGRESQL_CHECKPOINT_WRITE_TIME {0} {1}".format(writerStats['checkpoint_write_time'], _source))
-	print("POSTGRESQL_CHECKPOINTS_TIMED {0} {1}".format(writerStats['checkpoints_timed'], _source))
-	print("POSTGRESQL_BUFFERS_ALLOCATED {0} {1}".format(writerStats['buffers_alloc'], _source))
-	print("POSTGRESQL_BUFFERS_CLEAN {0} {1}".format(writerStats['buffers_clean'], _source))
-	print("POSTGRESQL_BUFFERS_BACKEND_FSYNC {0} {1}".format(writerStats['buffers_backend_fsync'], _source))
-	print("POSTGRESQL_CHECKPOINT_SYNCHRONIZATION_TIME {0} {1}".format(writerStats['checkpoint_sync_time'], _source))
-	print("POSTGRESQL_CHECKPOINTS_REQUESTED {0} {1}".format(writerStats['checkpoints_req'], _source))
-	print("POSTGRESQL_BUFFERS_BACKEND {0} {1}".format(writerStats['buffers_backend'], _source))
-	print("POSTGRESQL_MAXIMUM_WRITTEN_CLEAN {0} {1}".format(writerStats['maxwritten_clean'], _source))
-	print("POSTGRESQL_BUFFERS_CHECKPOINT {0} {1}".format(writerStats['buffers_checkpoint'], _source))
+#region MultiplyInstanceSupport
+_connections = sys.argv[1]
+connections = _connections.split(',')
+if len(connections) <= 0:
+    sys.stderr.write(
+        'usage: {0} "host port database user password source,host port database user password source, <...>"\n'.format(
+            path.basename(sys.argv[0])))
+    sys.exit(1)
 
-	#Global DB Stats
-	print("POSTGRESQL_BLOCKS_READ {0} {1}".format(dbStats['totals']['blks_read'], _source))
-	print("POSTGRESQL_DISK_SIZE {0} {1}".format(dbStats['totals']['disk_size'], _source))
-	print("POSTGRESQL_TRANSACTIONS_COMMITTED {0} {1}".format(dbStats['totals']['xact_commit'], _source))
-	print("POSTGRESQL_TUPLES_DELETED {0} {1}".format(dbStats['totals']['tup_deleted'], _source))
-	print("POSTGRESQL_TRANSACTIONS_ROLLEDBACK {0} {1}".format(dbStats['totals']['xact_rollback'], _source))
-	print("POSTGRESQL_BLOCKS_HIT {0} {1}".format(dbStats['totals']['blks_hit'], _source))
-	print("POSTGRESQL_TUPLES_RETURNED {0} {1}".format(dbStats['totals']['tup_returned'], _source))
-	print("POSTGRESQL_TUPLES_FETCHED {0} {1}".format(dbStats['totals']['tup_fetched'], _source))
-	print("POSTGRESQL_TUPLES_UPDATED {0} {1}".format(dbStats['totals']['tup_updated'], _source))
-	print("POSTGRESQL_TUPLES_INSERTED {0} {1}".format(dbStats['totals']['tup_inserted'], _source))
-	print("POSTGRESQL_TUPLES_FETCHED {0} {1}".format(dbStats['totals']['tup_fetched'], _source))
-	
-poll()
+queries = []
+for connection in connections:
+    _connection = connection.split(' ')
+    if len(_connection) != 6:
+        sys.stderr.write('usage: {0} "host port database user password source"\n'.format(path.basename(sys.argv[0])))
+        sys.exit(1)
+    queries.append({
+        'host': _connection[0],
+        'port': _connection[1],
+        'database': _connection[2],
+        'user': _connection[3],
+        'password': _connection[4],
+        'source': _connection[5]
+    })
+#endregion MultiplyInstanceSupport
+
+def poll(_host, _port, _database, _user, _password, _source):
+    _dbconn = PgInfo(_host, _port, _database, _user, _password)
+    dbs = _dbconn.getDatabases()
+
+    #global stats
+    writerStats = _dbconn.getBgWriterStats()
+    dbLocks = _dbconn.getLockStatsMode()
+    dbStats = _dbconn.getDatabaseStats()  #also gives individual DB stats
+
+    #per db stats - not used in this version
+    #locksByDB=_dbconn.getLockStatsDB()
+    #connStats=_dbconn.getConnectionStats()
+
+    #print writerStats
+    #lock stats
+    print("POSTGRESQL_EXCLUSIVE_LOCKS {0} {1}".format(dbLocks['all']['Exclusive'], _source))
+    print("POSTGRESQL_ROW_EXCLUSIVE_LOCKS {0} {1}".format(dbLocks['all']['RowExclusive'], _source))
+    print("POSTGRESQL_SHARE_ROW_EXCLUSIVE_LOCKS {0} {1}".format(dbLocks['all']['ShareRowExclusive'], _source))
+    print("POSTGRESQL_SHARE_UPDATE_EXCLUSIVE_LOCKS {0} {1}".format(dbLocks['all']['ShareUpdateExclusive'], _source))
+    print("POSTGRESQL_SHARE_LOCKS {0} {1}".format(dbLocks['all']['Share'], _source))
+    print("POSTGRESQL_ACCESS_SHARE_LOCKS {0} {1}".format(dbLocks['all']['AccessShare'], _source))
+
+    #checkpoint/bgwriter stats
+    print("POSTGRESQL_CHECKPOINT_WRITE_TIME {0} {1}".format(writerStats['checkpoint_write_time'], _source))
+    print("POSTGRESQL_CHECKPOINTS_TIMED {0} {1}".format(writerStats['checkpoints_timed'], _source))
+    print("POSTGRESQL_BUFFERS_ALLOCATED {0} {1}".format(writerStats['buffers_alloc'], _source))
+    print("POSTGRESQL_BUFFERS_CLEAN {0} {1}".format(writerStats['buffers_clean'], _source))
+    print("POSTGRESQL_BUFFERS_BACKEND_FSYNC {0} {1}".format(writerStats['buffers_backend_fsync'], _source))
+    print("POSTGRESQL_CHECKPOINT_SYNCHRONIZATION_TIME {0} {1}".format(writerStats['checkpoint_sync_time'], _source))
+    print("POSTGRESQL_CHECKPOINTS_REQUESTED {0} {1}".format(writerStats['checkpoints_req'], _source))
+    print("POSTGRESQL_BUFFERS_BACKEND {0} {1}".format(writerStats['buffers_backend'], _source))
+    print("POSTGRESQL_MAXIMUM_WRITTEN_CLEAN {0} {1}".format(writerStats['maxwritten_clean'], _source))
+    print("POSTGRESQL_BUFFERS_CHECKPOINT {0} {1}".format(writerStats['buffers_checkpoint'], _source))
+
+    #Global DB Stats
+    print("POSTGRESQL_BLOCKS_READ {0} {1}".format(dbStats['totals']['blks_read'], _source))
+    print("POSTGRESQL_DISK_SIZE {0} {1}".format(dbStats['totals']['disk_size'], _source))
+    print("POSTGRESQL_TRANSACTIONS_COMMITTED {0} {1}".format(dbStats['totals']['xact_commit'], _source))
+    print("POSTGRESQL_TUPLES_DELETED {0} {1}".format(dbStats['totals']['tup_deleted'], _source))
+    print("POSTGRESQL_TRANSACTIONS_ROLLEDBACK {0} {1}".format(dbStats['totals']['xact_rollback'], _source))
+    print("POSTGRESQL_BLOCKS_HIT {0} {1}".format(dbStats['totals']['blks_hit'], _source))
+    print("POSTGRESQL_TUPLES_RETURNED {0} {1}".format(dbStats['totals']['tup_returned'], _source))
+    print("POSTGRESQL_TUPLES_FETCHED {0} {1}".format(dbStats['totals']['tup_fetched'], _source))
+    print("POSTGRESQL_TUPLES_UPDATED {0} {1}".format(dbStats['totals']['tup_updated'], _source))
+    print("POSTGRESQL_TUPLES_INSERTED {0} {1}".format(dbStats['totals']['tup_inserted'], _source))
+    print("POSTGRESQL_TUPLES_FETCHED {0} {1}".format(dbStats['totals']['tup_fetched'], _source))
+
+for query in queries:
+    poll(query['host'], query['port'], query['database'], query['user'], query['password'], query['source'])
 
 
